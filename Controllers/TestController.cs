b347c2cefaf534a9623e7e9b3cf3a0506e5960f4
vbedoyac4 <vivianabedoyac4@gmail.com>
2024-12-04 14:30:58 -0600

    First Commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpPost("concatenate")]
+        public IActionResult ConcatenateStrings(string firstString, string secondString)
+        {
+            if (string.IsNullOrEmpty(firstString) || string.IsNullOrEmpty(secondString))
+            {
+                return BadRequest("Both strings must be non-empty");
+            }
+
+            string result = firstString + secondString;
+            return Ok(result);  
+        }
+    }
+}
